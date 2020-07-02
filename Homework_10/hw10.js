fedd08fe2f76fe384879188bdb937ba2a9540773
Alexey <Alx170890@gmail.com>
2020-07-02 13:56:09 +0300

    added change homework_10

--- Homework_10/hw10.js
+++ Homework_10/hw10.js
@@ -26,4 +26,12 @@ function printNumbers() {
 }
 printNumbers();
 
-//c.
+//2. Promise
+function delay(ms) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(resolve, ms);
+  });
+}
+delay(3000).then(() => console.log("выполнилось через 3 секунды"));
+
+
--- Homework_10/notify/notify.js
+++ Homework_10/notify/notify.js
@@ -1,25 +1,13 @@
 let close = () => {
   let notify = document.querySelector(".notify");
   notify.classList.add("fadeOutUp");  
-  notify.classList.remove("fadeInDown"); 
-}
-
+  notify.classList.remove("fadeInDown");   
+};
 document.querySelector("button").addEventListener("click", () => {
   let notify = document.querySelector(".notify");
+  notify.hidden = false;
   notify.classList.add("fadeInDown");  
-  notify.classList.remove("fadeOutUp");
-  document.querySelector(".notify").hidden = false;  
-  setTimeout(close,3000);
-  });
-
-
-
-document.querySelector("span").addEventListener("click", close);
-
-
-
-// () => {
-//   let notify = document.querySelector(".notify");
-//   notify.classList.add("fadeOutUp");  
-//   notify.classList.remove("fadeInDown"); 
-// }
+  notify.classList.remove("fadeOutUp");  
+  setTimeout(close,3000);  
+})
+document.querySelector("span").addEventListener("click", close)
