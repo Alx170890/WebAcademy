f4e9b0c62547e5e32b7df04b89f5aa1c1e59b15f
Alexey <Alx170890@gmail.com>
2020-07-04 19:51:32 +0300

    changed homework_11

--- Homework_11/weather/js/weather.js
+++ Homework_11/weather/js/weather.js
@@ -1,58 +1,118 @@
-// const getWeatherCityNow = (city) => (fetch(`https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=en&units=metric&q=${city}`, {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-// 		"x-rapidapi-key": "2f3477a892msh1889542a56384f8p1d5c76jsne7a71c9b8ec3"
-// 	}
-// })
-// .then(response => (response.json()))
-// .then((res) => (res))
-// .catch(err => {
-// 	console.log(err);
-// })
-// );
-
-
-document.querySelector("button").addEventListener("click", () => {    
-    let temperature = 34;    
-    let pressure = 704;  
-    let wind = 1.8;  
-    let humidity = 88;  
-    let weather = "cloud"; 
-    let time = 0;                  
-    renderWeather(temperature, pressure, wind, humidity, weather, time);   
+// Получаем данные с сервера
+const getWeatherCityNow = (city) => (fetch(`https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=en&units=metric&q=${city}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": "2f3477a892msh1889542a56384f8p1d5c76jsne7a71c9b8ec3"
+	}
 })
+.then(response => (response.json()))
+.then((res) => {    
+    return res;
+})
+.catch(err => {
+	console.log(err);
+})
+);
 
-document.querySelector("input").addEventListener("keydown", event => {
-    if (event.keyCode === 13 ) {
-        let temperature = 34;    
-        let pressure = 704;  
-        let wind = 1.8;  
-        let humidity = 88;  
-        let weather = "cloud";  
-        let time = 0;                 
-        renderWeather(temperature, pressure, wind, humidity, weather, time);
+// Переводим время в нужный формат
+function getCurrentTimeFromStamp(time) {
+    let date = new Date(time);  
+    let timeStampCon = `${date.getHours()}.${date.getMinutes()}`;  
+    return +timeStampCon;
+}
+// Добавляем фокус на input
+document.querySelector("input").focus();
+
+// Добавляем события на клик по кнопке
+document.querySelector("button").addEventListener("click", () =>{    
+    const city = document.querySelector("input").value;
+    document.querySelector("input").blur();
+    if (city) {
+        getWeatherCityNow(city).then((res) => {   
+            if (res.message) {                
+                let close = () => {
+                    let notify = document.querySelector(".notify");
+                    notify.classList.add("fadeOutUp");  
+                    notify.classList.remove("fadeInDown");                    
+                  };                      
+                    let notify = document.querySelector(".notify");
+                    notify.hidden = false;
+                    notify.classList.add("fadeInDown");  
+                    notify.classList.remove("fadeOutUp");                    
+                    setTimeout(close,3000);                
+            } else {
+                let temperature = Math.round(res.main.temp);    
+                let pressure = res.main.pressure;  
+                let wind = Math.round(res.wind.speed);  
+                let humidity = res.main.humidity;  
+                let weather = res.weather[0].main; 
+                let time = res.dt;
+                let normTime = getCurrentTimeFromStamp(time);
+                // console.log(normTime);                
+                renderWeather(temperature, pressure, wind, humidity, weather, normTime);
+            }                
+        })
     }
 })
 
+// Добавляем события на по нажатию на enter
+document.querySelector("input").addEventListener("keydown", event => {
+    if (event.keyCode === 13 ) {
+        const city = document.querySelector("input").value;
+        document.querySelector("input").blur();
+        if (city) {
+            getWeatherCityNow(city).then((res) => {
+                if (res.message) {                    
+                    let close = () => {
+                        let notify = document.querySelector(".notify");
+                        notify.classList.add("fadeOutUp");  
+                        notify.classList.remove("fadeInDown");                         
+                      };                      
+                        let notify = document.querySelector(".notify");
+                        notify.hidden = false;
+                        notify.classList.add("fadeInDown");  
+                        notify.classList.remove("fadeOutUp");                         
+                        setTimeout(close,3000);                                 
+                } else {
+                    let temperature = Math.round(res.main.temp);    
+                    let pressure = res.main.pressure;  
+                    let wind = Math.round(res.wind.speed);  
+                    let humidity = res.main.humidity;  
+                    let weather = res.weather[0].main; 
+                    let time = res.dt;
+                    let normTime = getCurrentTimeFromStamp(time);
+                    // console.log(normTime);                
+                    renderWeather(temperature, pressure, wind, humidity, weather, normTime);
+                }                
+            })
+        }
+    }
+})
 
+// Выводи данные на экран
 const renderWeather = (temp, press, wnd, hmdt, wthr, tm) =>  {
     const temperature = document.querySelector(".cityWeather_temperature");
     const pressure = document.querySelector(".cityWeather_pressure");
     const wind = document.querySelector(".cityWeather_wind");
     const humidity = document.querySelector(".cityWeather_humidity");
     const weather = document.querySelector(".cityWeather_weatherIndicator");
-    let time = document.querySelector(".cityWeather");
-    let body = document.querySelector("body");
-    let imgWeather = document.querySelector("img");    
+    const time = document.querySelector(".cityWeather");
+    const body = document.querySelector("body");
+    const imgWeather = document.querySelector("img");
+    temperature.innerHTML = `${temp}`;
+    pressure.innerHTML = `${press} mm`;
+    wind.innerHTML = `${wnd} m/c`;
+    humidity.innerHTML = `${hmdt} %`;
+    weather.innerHTML = `${wthr}`;
     switch(wthr) {
-        case "rain":
+        case "Rain":
             imgWeather.src = "img/rain.png";
         break;
-        case "cloud":
+        case "Clouds":
             imgWeather.src = "img/cloud.png";
         break;
-        case "snow":
+        case "Snow":
             imgWeather.src = "img/snow.png";
         break;        
         default:            
@@ -61,36 +121,5 @@ const renderWeather = (temp, press, wnd, hmdt, wthr, tm) =>  {
     if (tm > 20 || tm < 8) {         
         time.classList.add("backgroungNight");
         body.style.background = "#5c6576"
-    }    
-    temperature.innerHTML = `${temp}`;
-    pressure.innerHTML = `${press} mm`;
-    wind.innerHTML = `${wnd} m/c`;
-    humidity.innerHTML = `${hmdt} %`;
-    weather.innerHTML = `${wthr}`;
+    }        
 };
-
-
-// document.querySelector("button").addEventListener("click", () =>{
-//     const city = document.querySelector("input").value;
-//     if (city) {
-//         getWeatherCityNow(city).then((res) => {
-//             console.log(res);
-//             let temperature = Math.round(res.main.temp);    
-//             let pressure = Math.round(res.main.temp);  
-//             let wind = Math.round(res.main.temp);  
-//             let humidity = Math.round(res.main.temp);  
-//             let weather = res.main.temp;                     
-//             renderWeather(temperature, pressure, wind, humidity, weather);
-//         })
-//     }
-// })
-
-
-
-
-
-
-
-
-
-
